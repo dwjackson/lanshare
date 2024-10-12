80c58bd3f4310df629206993a396db0acc12426f
David Jackson <dwjackson@users.noreply.github.com>
2024-10-12 18:15:49 -0400

    Sort file names alphabetically

--- page.go
+++ page.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"html/template"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -19,13 +20,18 @@ func WritePage(path string, files []os.FileInfo) string {
 	t := template.Must(template.New("pageHtml").Parse(pageHtml))
 	b := strings.Builder{}
 	var links []Link
-	if path != "." {
-		links = append(links, upDir(path))
-	}
 	for _, fi := range files {
 		link := linkFromFileInfo(path, fi)
 		links = append(links, link)
 	}
+	sort.Slice(links, func(i, j int) bool {
+		return links[i].Name < links[j].Name
+	})
+	upSlice := []Link { upDir(path) }
+	if path != "." {
+		links = append(upSlice, links...)
+	}
+	
 	var webPath string
 	if path == "." {
 		webPath = "/"
